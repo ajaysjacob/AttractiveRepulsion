b0a6309bd12ff502a30c78aadc6622dba8dc63e7
ajaysjacob <ajaysjacob@gmail.com>
2017-02-08 01:04:42 +0530

    Initilisation of Game Object and Atrractor and Repulsor GameObjects Along With Corresponding scripts so as to start the game and simulate Attraction and Repulsion

---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStart : MonoBehaviour {
+
+	public GameObject Player= GameObject.FindGameObjectWithTag ("Player");
+	public Rigidbody Plyr;
+	void Start () {
+		Player= (GameObject) GameObject.FindGameObjectWithTag ("Player");
+		Plyr= Player.GetComponent<Rigidbody>();
+	}
+
+
+	void Update () {
+		if (Input.GetButtonDown("Launch")) {
+			Plyr.velocity= new Vector3(1,0,0);
+		}
+
+	}
+}
---
+++ Assets/Scripts/Repulsor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Repulsor : MonoBehaviour {
+
+
+		public float a;
+		public GameObject Player;
+		public Rigidbody Plyr;
+		void Start () {
+			Player = GameObject.FindGameObjectWithTag ("Player");
+			Plyr = Player.GetComponent<Rigidbody>();
+		}
+
+		void Update () {
+			if (Player.transform.position != Vector3.zero) {
+				Vector3 r = (Vector3) Player.transform.position - transform.position;
+				Plyr.AddForce ((a / (Mathf.Pow (Vector3.Magnitude (r), 3))) * r);
+			} 
+		}
+}
